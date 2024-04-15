e378e2faf832a621c98f239e0b41e3ea99e94720
Marcella Carneiro <marcella.carneiro@a.fecaf.com.br>
2024-04-14 23:51:14 -0300

    finalizando projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Olá, seja bem vindo. Para prosseguir, insira seus dados");
+        
+        System.out.println("Nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Número: ");
+        int numero = sc.nextInt();
+
+        System.out.println("Agência: ");
+        String agencia = sc.next();
+
+        System.out.println("Saldo: ");
+        double saldo = sc.nextDouble();
+
+        sc.close();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
